e4020de6a1d55063c3d339f01bcb1e4cf9b80028
kelsey-s <58612892+kelsey-s@users.noreply.github.com>
2020-09-13 15:37:53 -0400

    Add files via upload
    Download the folder and run R file using R or R Studio.

---
+++ WebPage/server.R
@@ -0,0 +1,10 @@
+library(shiny)
+
+# Define server logic required to plot various variables against mpg
+server <-function(input, output,session) {
+  
+}
+
+
+
+
---
+++ WebPage/ui.r
@@ -0,0 +1,10 @@
+ui<-fluidPage(tags$div(img(src="picture1.png",width="1445px",height="877px",
+                                    style="position:relative;left:-1px;top:0;")
+                        )
+                       ,
+              tags$h1(align="center",style="front-family:'Abadi';font-size:90px;color:#FFFFFF;position:absolute;top:180px;right:470px","CP DELICACY"),
+              tags$h2(align="center",style="front-family:'Abadi';font-size:65px;color:#FFFFFF;position:absolute;top:300px;right:330px","We try to delight your day!"),
+              tags$a(align='center',style="front-family:'Abadi';font-size:40px;color:#FFFFFF;position:absolute;top:460px;right:870px",
+                herf="www,http://rstudio.com","Users "),
+              tags$a(align='center',style="front-family:'Abadi';font-size:40px;color:#FFFFFF;position:absolute;top:460px;right:400px",herf="www,http://rstudio.com","Potential Owners "),
+              tags$p(align='center',style="front-family:'times';font-size:20px;color:#FFFFFF;position:absolute;top:830px;right:500px",'@2019 CP DELICACY. All Rights Reserved.| Contact Us'))
